3a84db25e6e55e75b8a06a20fb1f77f954dfd78d
Akshay Kumar <akshay.nm92@gmail.com>
2021-04-03 00:37:08 +0530

    Setup GET and POST requests in components

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,67 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useCallback, useEffect } from 'react'
+const { confetch, configure, getUrlFromPath } = require('@akshay-nm/confetch')
 
-export default function Home() {
+const ComponentDemoForPostRequest = () => {
+  const sendPostRequest = useCallback(() => {
+    const request = confetch({
+      url: getUrlFromPath('/api'),
+      method: 'POST',
+      body: JSON.stringify({ your: 'payload' }),
+      headers: {
+        your: 'headers',
+        'Content-Type': 'application/json',
+      },
+    })
+    request.send().catch((error) => {
+      console.log('Error while trying to send POST request: ', error)
+    })
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <button onClick={sendPostRequest}>Send a POST request</button>
     </div>
   )
 }
+
+const ComponentDemoForGetRequest = () => {
+  const sendPostRequest = useCallback(() => {
+    const request = confetch({
+      url: getUrlFromPath('/api'),
+      method: 'GET',
+      headers: {
+        your: 'headers',
+      },
+    })
+    request.send().catch((error) => {
+      console.log('Error while trying to send GET request: ', error)
+    })
+  }, [])
+
+  return (
+    <div>
+      <button onClick={sendPostRequest}>Send a GET request</button>
+    </div>
+  )
+}
+
+const App = () => {
+  useEffect(() => {
+    configure({
+      baseUrl: 'http://localhost:3000',
+      debug: true,
+      timeoutDuration: 1000,
+      headers: {
+        default: 'headers',
+      },
+    })
+  }, [])
+  return (
+    <div>
+      <ComponentDemoForGetRequest />
+      <ComponentDemoForPostRequest />
+    </div>
+  )
+}
+
+export default App
